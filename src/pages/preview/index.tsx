484cc52abcdd85e7c311d8934796d02e91804061
eqypo9 <eqypo@naver.com>
2024-11-06 02:23:50 +0900

    Remove: Preview.module.scss

--- src/pages/preview/index.tsx
+++ src/pages/preview/index.tsx
@@ -1,39 +1,38 @@
 import IconComponent from '@/components/Asset/Icon';
-import styles from './Preview.module.scss';
 import ToggleButton from '@/components/ToggleButton';
 import Header from '@/components/Layout/Header';
 
 export default function Preview() {
   return (
-    <div className={styles.container}>
-      <h2 className={styles.title}>Icon Preview</h2>
+    <div>
+      <h2>Icon Preview</h2>
 
       <div>
-        <p className={styles.iconDescription}>Sizes: Small, Medium, Large</p>
-        <div className={styles.iconGroup}>
+        <p>Sizes: Small, Medium, Large</p>
+        <div>
           <IconComponent name="search" size="s" />
           <IconComponent name="search" size="m" />
           <IconComponent name="search" size="l" />
         </div>
       </div>
 
-      <hr className={styles.section} />
+      <hr />
 
       <div>
-        <p className={styles.iconDescription}>Custom Sizes:</p>
-        <div className={styles.iconGroup}>
+        <p>Custom Sizes:</p>
+        <div>
           <IconComponent name="blueHeartBlank" width={36} height={36} />
           <IconComponent name="kakao" alt="Kakao logo" width={48} height={48} />
         </div>
       </div>
 
-      <div className={styles.toggleContainer}>
-        <p className={styles.title}>Toggle Button</p>
+      <div>
+        <p>Toggle Button</p>
         <ToggleButton />
       </div>
 
-      <div className={styles.headerContainer}>
-      <p className={styles.title}>Header</p>
+      <div>
+        <p>Header</p>
         <Header />
       </div>
     </div>
