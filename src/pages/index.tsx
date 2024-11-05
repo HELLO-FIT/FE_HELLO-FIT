098aa151be2b55c5050fe8ad71b6e401dafc4344
MEGUMMY1 <cys10902@nate.com>
2024-11-05 17:12:28 +0900

    Design: favicon 변경

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,7 @@ export default function Home() {
         <title>헬로핏</title>
         <meta name="description" content="Hello Fit" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="../../public/favicon.ico" />
       </Head>
     </>
   );
