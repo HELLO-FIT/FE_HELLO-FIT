fcca3a1d65db62b88520a4de278ec16a99e42968
MEGUMMY1 <cys10902@nate.com>
2024-11-08 03:31:01 +0900

    Feat: 지도 확대 조회 페이지

---
+++ src/pages/details/[id]/map/index.tsx
@@ -0,0 +1,19 @@
+import { useRouter } from 'next/router';
+import data from '@/components/Schedule/temp.json';
+import Subheader from '@/components/Layout/Subheader';
+import LargeMap from '@/components/CourseDetails/LargeMap';
+import ButtonContainer from '@/components/ButtonContainer';
+
+export default function map() {
+  const router = useRouter();
+  const { id } = router.query;
+
+  const Id = data.find(item => item.id === Number(id));
+  return (
+    <>
+      <Subheader subheaderText="강좌 상세" isGray={false} />
+      {Id && <LargeMap address={Id.address} />}
+      <ButtonContainer />
+    </>
+  );
+}
