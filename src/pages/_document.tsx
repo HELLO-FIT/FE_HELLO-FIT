2a73d08dada18b67298fb6c252a6945d7f1f170a
MEGUMMY1 <cys10902@nate.com>
2024-11-08 15:20:12 +0900

    Fix: App key 상수처리

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,14 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
+  const KAKAO_MAP_KEY = process.env.NEXT_PUBLIC_KAKAO_MAP_APP_KEY;
   return (
     <Html lang="en">
       <Head>
         <script
           async
           type="text/javascript"
-          src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAP_APP_KEY}&libraries=services,clusterer,drawing&autoload=false`}
+          src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${KAKAO_MAP_KEY}&libraries=services,clusterer,drawing&autoload=false`}
         ></script>
       </Head>
       <body>
