49528c9b39b41fa0f885513ca6d838c8fef6fbf6
MEGUMMY1 <cys10902@nate.com>
2024-11-08 22:41:44 +0900

    Feat: 카드 컴포넌트 분리

--- src/components/CourseDetails/SpecialInfoCard/index.tsx
+++ src/components/CourseDetails/SpecialInfoCard/index.tsx
@@ -9,15 +9,15 @@ export default function SpecialInfoCard({
 }: SpecialInfoCardProps) {
   return (
     <section className={styles.box}>
-      <p className={styles.labelValueText}>
+      <div className={styles.labelValueText}>
         <span className={styles.label}>장애 특수 지도자</span>
         <span className={styles.value}>{coach}명</span>
-      </p>
-      <p className={styles.labelValueText}>
+      </div>
+      <div className={styles.labelValueText}>
         <span className={styles.label}>차량 지원</span>
         <span className={styles.value}>{vehicle ? '지원' : '없음'}</span>
-      </p>
-      <p className={styles.labelValue}>
+      </div>
+      <div className={styles.labelValue}>
         <span className={styles.label}>장애 유형</span>
         <div className={styles.buttonContainer}>
           {specialType.map((type, index) => (
@@ -26,8 +26,8 @@ export default function SpecialInfoCard({
             </button>
           ))}
         </div>
-      </p>
-      <p className={styles.labelValue}>
+      </div>
+      <div className={styles.labelValue}>
         <span className={styles.label}>편의 시설</span>
         <div className={styles.buttonContainer}>
           {amenities.map((facility, index) => (
@@ -36,7 +36,7 @@ export default function SpecialInfoCard({
             </button>
           ))}
         </div>
-      </p>
+      </div>
     </section>
   );
 }
