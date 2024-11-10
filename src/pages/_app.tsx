baa0481e36528af6b12a9b6fe151a64bb3389d3e
MEGUMMY1 <cys10902@nate.com>
2024-11-10 23:55:41 +0900

    Chore: react-query, axios 설치

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,12 @@
 import type { AppProps } from 'next/app';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import '@/styles/globals.scss';
 import '@/styles/reset.css';
 import Header from '@/components/Layout/Header';
 import { useRouter } from 'next/router';
 import GNB from '@/components/Layout/GNB';
+
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
@@ -27,16 +30,18 @@ export default function App({ Component, pageProps }: AppProps) {
   const withoutHeader = ['/', '/login'].includes(router.pathname);
 
   return (
-    <div className="body">
-      <div
-        className={`appContainer ${!hideGNB ? 'withGNB' : ''} ${
-          withoutHeader ? 'withoutHeader' : ''
-        }`}
-      >
-        {!hideHeader && <Header />}
-        <Component {...pageProps} />
-        {!hideGNB && <GNB />}
+    <QueryClientProvider client={queryClient}>
+      <div className="body">
+        <div
+          className={`appContainer ${!hideGNB ? 'withGNB' : ''} ${
+            withoutHeader ? 'withoutHeader' : ''
+          }`}
+        >
+          {!hideHeader && <Header />}
+          <Component {...pageProps} />
+          {!hideGNB && <GNB />}
+        </div>
       </div>
-    </div>
+    </QueryClientProvider>
   );
 }
