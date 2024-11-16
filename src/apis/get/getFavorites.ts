0b880681ad5676f978e19c23c9b6334df67cae62
MEGUMMY1 <cys10902@nate.com>
2024-11-16 22:25:36 +0900

    Fix: 찜 목록 담은 순서대로 쌓이게 수정

--- src/apis/get/getFavorites.ts
+++ src/apis/get/getFavorites.ts
@@ -17,7 +17,7 @@ export interface FavoritesItem {
 export async function getFavorites(): Promise<FavoritesItem[]> {
   try {
     const response = await BASE_URL.get<FavoritesItem[]>('/users/favorites');
-    return response.data;
+    return response.data.reverse();
   } catch (error) {
     console.error('Error fetching Favorites:', error);
     throw new Error('Failed to fetch Favorites');
