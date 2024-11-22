3a91bd9e80725cfe6cc18d663ea90ba3b8146336
MEGUMMY1 <cys10902@nate.com>
2024-11-22 13:25:19 +0900

    Feat: 특수 유형 검색 구현

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -5,6 +5,8 @@ import {
   NomalFacility,
   getNomalFacilities,
   GetNomalFacilitiesParams,
+  SpecialFacility,
+  getSpecialFacilities,
 } from '@/apis/get/getFacilities';
 import styles from './Search.module.scss';
 import SearchBar from './SearchBar';
@@ -12,46 +14,63 @@ import Link from 'next/link';
 import Schedule from '../Schedule';
 import IconComponent from '../Asset/Icon';
 import { selectedLocalCodeState } from '@/states/filterState';
+import { toggleState } from '@/states/toggleState';
 
 export default function Search() {
   const router = useRouter();
   const { query } = router.query;
-  const [facilities, setFacilities] = useState<NomalFacility[]>([]);
-  const [filteredFacilities, setFilteredFacilities] = useState<NomalFacility[]>(
-    []
-  );
+  const [facilities, setFacilities] = useState<
+    NomalFacility[] | SpecialFacility[]
+  >([]);
+  const [filteredFacilities, setFilteredFacilities] = useState<
+    NomalFacility[] | SpecialFacility[]
+  >([]);
   const selectedLocalCode = useRecoilValue(selectedLocalCodeState);
+  const toggle = useRecoilValue(toggleState);
 
+  // 시설 목록을 가져오는 함수
   useEffect(() => {
     const fetchFacilities = async () => {
       try {
         const params: GetNomalFacilitiesParams = {
           localCode: selectedLocalCode,
         };
-        const fetchedFacilities = await getNomalFacilities(params);
-        setFacilities(fetchedFacilities);
+
+        if (toggle === 'special') {
+          const fetchedFacilities = await getSpecialFacilities({
+            localCode: selectedLocalCode,
+          });
+          setFacilities(fetchedFacilities);
+        } else {
+          const fetchedFacilities = await getNomalFacilities(params);
+          setFacilities(fetchedFacilities);
+        }
       } catch (error) {
         console.error('시설 데이터를 불러오는 데 실패했습니다:', error);
       }
     };
 
     fetchFacilities();
-  }, [selectedLocalCode]);
+  }, [selectedLocalCode, toggle]);
 
+  // 검색 필터 적용
   useEffect(() => {
     if (query && facilities.length > 0) {
       const lowerCaseQuery = String(query).toLowerCase();
 
       const filtered = facilities.filter(facility => {
-        // 검색 조건: name, address, detailAddress, items, owner
+        // 검색 조건: name, address, detailAddress, items
         return (
-          facility.name.toLowerCase().includes(lowerCaseQuery) ||
-          facility.address.toLowerCase().includes(lowerCaseQuery) ||
-          facility.detailAddress?.toLowerCase().includes(lowerCaseQuery) ||
-          facility.items.some(item =>
-            item.toLowerCase().includes(lowerCaseQuery)
-          ) ||
-          facility.owner.toLowerCase().includes(lowerCaseQuery)
+          ('name' in facility &&
+            facility.name.toLowerCase().includes(lowerCaseQuery)) ||
+          ('address' in facility &&
+            facility.address.toLowerCase().includes(lowerCaseQuery)) ||
+          ('detailAddress' in facility &&
+            facility.detailAddress?.toLowerCase().includes(lowerCaseQuery)) ||
+          ('items' in facility &&
+            facility.items.some(item =>
+              item.toLowerCase().includes(lowerCaseQuery)
+            ))
         );
       });
 
@@ -67,8 +86,8 @@ export default function Search() {
         <ul className={styles.scheduleContainer}>
           {filteredFacilities.map(facility => (
             <Link
-              key={`${facility.businessId}-${facility.serialNumber}`}
-              href={`/details/${facility.businessId}/${facility.serialNumber}`}
+              key={`${facility.businessId}-${'serialNumber' in facility ? `/${facility.serialNumber}` : ''}`}
+              href={`/details/${facility.businessId}${'serialNumber' in facility ? `/${facility.serialNumber}` : ''}`}
             >
               <Schedule facility={facility} />
             </Link>
@@ -77,7 +96,7 @@ export default function Search() {
       ) : (
         <div className={styles.resultContainer}>
           <IconComponent
-            name="noResult"
+            name={toggle === 'general' ? 'noResult' : 'noResultSP'}
             width={48}
             height={48}
             alt="결과 없음"
