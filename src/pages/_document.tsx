9a324d2e4715fb4e5d3831338c719d8922aa0c7d
MEGUMMY1 <cys10902@nate.com>
2024-11-23 13:58:15 +0900

    Feat: Favicon 설정

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,35 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="icon" href="../../public/favicon.ico" />
+        <link rel="icon" href="../../public/favicon/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          href="../../public/favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href="../../public/favicon/android-chrome-192x192.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="512x512"
+          href="../../public/favicon/android-chrome-512x512.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="../../public/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="../../public/favicon/favicon-16x16.png"
+        />
         <script
           defer
           src="https://developers.kakao.com/sdk/js/kakao.min.js"
