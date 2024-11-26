39b722d8cfe00638536122342e7dbb59d95d1c46
eqypo9 <eqypo@naver.com>
2024-11-26 21:48:14 +0900

    Feat: Toggle Slider

--- src/components/Button/ToggleButton/index.tsx
+++ src/components/Button/ToggleButton/index.tsx
@@ -31,16 +31,34 @@ export default function ToggleButton({ onButtonClick }: ToggleButtonProps) {
   };
 
   return (
-    <div className={styles.toggle}>
+    <div
+      className={styles.toggle}
+      onClick={() =>
+        handleClick(activeButton === 'general' ? 'special' : 'general')
+      }
+    >
+      <div
+        className={styles.slider}
+        style={{
+          transform:
+            activeButton === 'special' ? 'translateX(100%)' : 'translateX(0)',
+        }}
+      />
       <button
         className={`${styles.toggleOption} ${activeButton === 'general' ? styles.active : ''}`}
-        onClick={() => handleClick('general')}
+        onClick={e => {
+          e.stopPropagation();
+          handleClick('general');
+        }}
       >
         일반
       </button>
       <button
         className={`${styles.toggleOption} ${activeButton === 'special' ? `${styles.active} ${styles.specialActive}` : ''}`}
-        onClick={() => handleClick('special')}
+        onClick={e => {
+          e.stopPropagation();
+          handleClick('special');
+        }}
       >
         특수
       </button>
